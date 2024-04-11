fc6a2ec3c46055af61b8361ddf365ade4a62b714
Farbod909 <rafezyfarbod@gmail.com>
2024-04-10 22:18:33 -0700

    Prettify new code

--- src/controllers/movementController.ts
+++ src/controllers/movementController.ts
@@ -1,30 +1,40 @@
 import asyncHandler from 'express-async-handler';
+import { Request, Response, NextFunction } from 'express';
 
-const movement_list = asyncHandler(async (req, res, _next) => {
-  res.send("NOT IMPLEMENTED: LIST");
-});
+const movement_list = asyncHandler(
+  async (req: Request, res: Response, _next: NextFunction) => {
+    res.send('NOT IMPLEMENTED: LIST');
+  },
+);
 
-const movement_detail = asyncHandler(async (req, res, _next) => {
-  res.send("NOT IMPLEMENTED: DETAIL");
-});
+const movement_detail = asyncHandler(
+  async (req: Request, res: Response, _next: NextFunction) => {
+    res.send('NOT IMPLEMENTED: DETAIL');
+  },
+);
 
-const movement_create = asyncHandler(async (req, res, _next) => {
-  res.send("NOT IMPLEMENTED: CREATE");
-});
+const movement_create = asyncHandler(
+  async (req: Request, res: Response, _next: NextFunction) => {
+    res.send('NOT IMPLEMENTED: CREATE');
+  },
+);
 
-const movement_update = asyncHandler(async (req, res, _next) => {
-  res.send("NOT IMPLEMENTED: UPDATE");
-});
+const movement_update = asyncHandler(
+  async (req: Request, res: Response, _next: NextFunction) => {
+    res.send('NOT IMPLEMENTED: UPDATE');
+  },
+);
 
-const movement_delete = asyncHandler(async (req, res, _next) => {
-  res.send("NOT IMPLEMENTED: DELETE");
-});
-
+const movement_delete = asyncHandler(
+  async (req: Request, res: Response, _next: NextFunction) => {
+    res.send('NOT IMPLEMENTED: DELETE');
+  },
+);
 
 export {
   movement_list,
   movement_detail,
   movement_create,
   movement_update,
-  movement_delete
+  movement_delete,
 };
